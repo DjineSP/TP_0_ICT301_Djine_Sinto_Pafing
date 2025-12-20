3f785e380ed9ee1eb7ac8653d8a6f071d2919332
DjineSP <dsintopafing@gmail.com>
2025-12-20 20:28:16 +0100

    DIP: ajout de l'exemple avant refactoring (dépendance forte à MySQLDatabase)

---
+++ DIP/avant_refactoring/Main.java
@@ -0,0 +1,8 @@
+package DIP.avant_refactoring;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderProcessor orderProcessor = new OrderProcessor();
+        orderProcessor.processOrder("'Données à sauvegarder'");
+    }
+}
---
+++ DIP/avant_refactoring/MySQLDatabase.java
@@ -0,0 +1,7 @@
+package DIP.avant_refactoring;
+
+public class MySQLDatabase {
+    public void save(String data) {
+        System.out.println("Saving to MySQL: " + data);
+    }
+}
